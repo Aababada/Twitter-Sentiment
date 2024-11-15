e9bf5215b01062e6350ef6000bf3328c0d68a0eb
Aanand Ingle <aanandingle22@gmail.com>
2024-11-15 11:22:11 +0530

    Update Twitter.py

--- Twitter.py
+++ Twitter.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-df = pd.read_csv('D:/twitter.csv')
+df = pd.read_csv('twitter_data.csv')
 print(df.columns)
 df['Sentiment'] = df['Sentiment'].astype('category')
 sns.countplot(x='Sentiment', data=df)
